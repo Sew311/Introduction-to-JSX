a9aedb0e9ca446de66086c0dc1d42ecae5d79ceb
Sew311 <sewwandia311@gmail.com>
2026-01-08 10:17:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var react = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(<div>
+    <h1>Hello World!</h1>
+    <p>
+        this is a paragraph.
+    </p>
+</div>
+ documnet.getElementById("root"));
